ed9a20356c6f68ee60f12677d9c3343495029d57
LLFish <1307557467@qq.com>
2023-09-08 20:11:19 +0800

    bugfix

--- service_worker.js
+++ service_worker.js
@@ -1,6 +1,6 @@
 const version = 1;
 const cacheName = `font-cache-v${version}`;
-const fetch_later = ['.html'];
+const fetch_later = ['.html', 'github.svg'];
 const black_list = ['/repos/LLFish163/youyou_comments/issues'];
 
 self.addEventListener('install', function (event) {
